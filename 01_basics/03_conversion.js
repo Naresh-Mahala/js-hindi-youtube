c25ee9de8ffa77bb9a89e0c1dc1fdd2f6aa164fb
Naresh-Mahala <nareshmahala2004@gmail.com>
2026-09-14 16:21:28 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,37 @@
+//documentation ke liye reference=   tc39 & mdn
+
+/*
+let score ="98"
+
+console.log(typeof score);
+
+let IntScore= Number(score);
+console.log(typeof IntScore);
+console.log(IntScore);
+*/
+
+/*
+let first=87
+let second=""
+let third= null;
+ 
+console.table([typeof first,typeof second,typeof third]);
+
+let boolfirst= Boolean(first)
+let boolsecond= Boolean(second)
+let boolthird= Boolean(third)
+
+console.table([typeof boolfirst,typeof boolsecond,typeof boolthird]);
+console.table([boolfirst,boolsecond,boolthird]);
+
+*/
+
+let num=100;
+ 
+let stringNum= String(num);
+
+console.log(num);
+console.log(stringNum);
+console.log(typeof num);
+console.log(typeof stringNum);
+
