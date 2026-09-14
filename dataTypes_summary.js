011a6a01eab9f98c2a7d652007f1e7a21d944be2
Naresh-Mahala <nareshmahala2004@gmail.com>
2026-09-14 17:27:10 +0000

    datatypes and memory

--- dataTypes_summary.js
+++ dataTypes_summary.js
@@ -3,7 +3,8 @@
 
 //reference (non primitive)
 // array, function, objects
-
+  
+/*
 const heros=["naresh","samay","vicky"]
 
 let myObj={
@@ -19,5 +20,25 @@ console.log(typeof myFunction);
 console.log(typeof heros);
 console.log(typeof myObj);
 
+*/
 
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//stack(primitive)     heap(non-primitive)
+ // copy bnega           reference milega
 
+ let obj_one={
+    name:"naresh",
+    age:22
+ }
+
+ let obj_two= obj_one
+
+ console.log(obj_one);
+ console.log(obj_two);
+ 
+ 
+ obj_two.name="mahala";
+ obj_two.age=23;
+ 
+ console.log(obj_one);
+ console.log(obj_two);
