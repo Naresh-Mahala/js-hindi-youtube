c107c6d00ae09098d3b704c535ed09b26a99f2d2
Naresh-Mahala <nareshmahala2004@gmail.com>
2026-09-14 15:32:44 +0000

    vari

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId= 369
+let emailId="naresh@gamil.com"
+var password="1111" //var use na kre....scope releted problem se bachne ke liye..
+
+accountCity="bikaner"; //semicolon lgaao ya na lgaao... koi frk nhi pdta..
+let accountState;
+
+// accountId=69 //error aayega
+
+console.log(accountId)
+
+console.table([accountId,emailId,password,accountCity,accountState])
+
+emailId="radha@gamil.com"
+ password="11"
+accountCity="nokha";
+accountState = "raj";
+
+console.table([accountId,emailId,password,accountCity,accountState])
