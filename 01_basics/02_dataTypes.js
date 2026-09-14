0973ca9cff9cd2f6cb67348275b1d5f64cb83db1
Naresh-Mahala <nareshmahala2004@gmail.com>
2026-09-14 15:57:48 +0000

    data_types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";  //treat all js as new version -> good practice
+
+// alert(3+3)  //this is node.js , not a browser
+
+console.log("naresh");
+// console.log(naresh); // not defined
+
+let name ="naru"
+let age=22;
+let gentlemen=true;
+let state;
+let gf=null;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof gentlemen);
+console.log(typeof state); //undefined
+console.log(typeof gf); //object
+
+console.log(typeof 22);
+console.log(typeof true);
+console.log(typeof null);
+
+
