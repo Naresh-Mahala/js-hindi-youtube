606d9050312e90719a2ab216127ab7884072946f
Naresh-Mahala <nareshmahala2004@gmail.com>
2026-09-14 17:16:29 +0000

    data types summary

---
+++ dataTypes_summary.js
@@ -0,0 +1,23 @@
+//primitive
+// 7 types-> string, number, boolean,null, undefined symbol, Bigint
+
+//reference (non primitive)
+// array, function, objects
+
+const heros=["naresh","samay","vicky"]
+
+let myObj={
+    name:"naresh",
+    age:22,
+}
+
+const myFunction= function(){
+    console.log("Hello World");
+    
+}
+console.log(typeof myFunction);
+console.log(typeof heros);
+console.log(typeof myObj);
+
+
+
