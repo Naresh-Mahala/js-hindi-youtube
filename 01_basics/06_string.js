8feb5db7b4c80c7efd34b8562568ced404a18bf9
Naresh-Mahala <nareshmahala2004@gmail.com>
2026-09-15 14:45:13 +0000

    string

---
+++ 01_basics/06_string.js
@@ -0,0 +1,20 @@
+const name= "Naresh"
+const repoCount= 3;
+
+// console.log(name + repoCount + " hello");  outdated
+
+console.log(`My name is ${name} and number of repo is ${repoCount} .........`);  //use this
+
+const name2= new String('Mahala'); // string ko declare krne ka naya tareeka...ese declare krne se bahut sare methods mil jaate hai...inbuilt type
+ 
+// console.log(name2[0]);
+// console.log(name2.__proto__)
+// console.log(name2.toUpperCase());
+
+console.log(name2.charAt(1));
+console.log(name2.charAt(6));
+
+console.log(name2.indexOf('M'));
+console.log(name2.indexOf('a'));
+
+//or bhi bahut methods hai...mujhe likhne ka mann nhi h na yaad krne ka .. to next pdte hai..
