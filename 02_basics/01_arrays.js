ff711d53511d4c53f2e800231f64a1d95d0b6398
Naresh-Mahala <nareshmahala2004@gmail.com>
2026-09-15 17:16:11 +0000

    arr

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,9 @@
+// const myArr=[1,2,3,4,"naresh",true] // yaha kuch bhi chalta hai
+
+const myArr=[1,2,3,4,5];
+
+const myArr2=new Array(1,2,3,4,5);//ese bhi declare krdo
+
+console.log(myArr[0]);
+
+//phir se baht saare methods... push, pop, unshift, shift, slice, splice...... 
