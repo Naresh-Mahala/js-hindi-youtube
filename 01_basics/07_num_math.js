0c5247fd8c43da0aec15992a90c5000728890aab
Naresh-Mahala <nareshmahala2004@gmail.com>
2026-09-15 15:12:01 +0000

    hi

---
+++ 01_basics/07_num_math.js
@@ -0,0 +1,23 @@
+const score=400
+
+// console.log(score);
+
+const score2 = new Number(40)
+
+// console.log(score2);
+
+// console.log(score2.toFixed(3)); //decimal ke baad kaha tak
+
+//fir se function ka rataafication....  nhi likhna mujhe
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min=10
+const max=40
+console.log(Math.floor(Math.random()*(max-min)+1) + min);//hamesa 10 or bich mei hi dega
+
+
+
+
